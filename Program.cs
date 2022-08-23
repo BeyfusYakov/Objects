054d9f045759f85304183840fb15a217f920614f
bejaw <beyfus.Yakov@gmail.com>
2022-08-23 21:07:03 +0700

    21,06

--- Program.cs
+++ Program.cs
@@ -1,6 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore;
+using Objects;
 
-app.MapGet("/", () => "Hello World!");
+namespace objects { 
+public class program {
 
-app.Run();
+        public static void Main(String[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+
+            ////////////////////////////////////////////////////////
+
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            WebApplication app = builder.Build();
+
+            //WebApplication app = WebApplication.CreateBuilder(args).Build();
+            //
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapGet("/", () => "Мы в режиме разработки");
+                app.MapGet("/hi", () => "Мы приветствуем тебя");
+            }
+            else
+            {
+                app.MapGet("/", () => "Hello World!");
+            }
+
+            app.Run();
+            //app.Start();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+           return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
+
+    }
+
+}
