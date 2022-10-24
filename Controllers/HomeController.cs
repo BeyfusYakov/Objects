8ec15ff623b0ca3b27f8151772cbded5770d21fe
Яков Бейфусь <bejaw2@gmail.com>
2022-10-24 18:31:24 +0700

    24.10.2022

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Objects.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+            data.Add("Ключ", "Значение");
+            return View(data);
+        }
+    }
+}
